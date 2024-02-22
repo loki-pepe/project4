182322d3dc0c4dead608244690be93c04a981c21
loki-pepe <lovroperaic@gmail.com>
2024-02-22 14:53:50 +0100

    add id field to User model

--- network/models.py
+++ network/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.BigAutoField(primary_key=True)
 
 class Post(models.Model):
     id = models.BigAutoField(primary_key=True)
