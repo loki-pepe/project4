a6386b73104ca2d2391813613aa30fd0765fe751
loki-pepe <lovroperaic@gmail.com>
2024-02-22 14:47:47 +0100

    create Post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.CharField(blank=False, null=False, max_length=300)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["creator"])
+        ]
+        ordering = ["timestamp"]
